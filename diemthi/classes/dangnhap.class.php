e9aa334ba093d3c097d55f77985fe4b7f14ce29f
LAS_Chanh <thanhnguyenhoang171official@gmail.com>
2024-03-13 13:45:17 +0700

    Web for testing

---
+++ diemthi/classes/dangnhap.class.php
@@ -0,0 +1,31 @@
+<?php
+require "DB.class.php";
+class dangnhap extends DB
+{
+    function alluser()
+    {
+        $con = $this->connect();
+        $mysql = "select * from user";
+        $query = mysqli_query($con, $mysql);
+        $result = array();
+        if ($query) {
+            while ($row = mysqli_fetch_assoc($query)) {
+                $result[] = $row;
+            }
+        }
+        return $result;
+    }
+    function selectuser($id, $pass)
+    {
+        $con = $this->connect();
+        $mysql = "select * from user where userid={$id} and password={$pass}";
+        $query = mysqli_query($con, $mysql);
+        $result = array();
+        if (mysqli_num_rows($query) > 0) {
+            $row = mysqli_fetch_assoc($query);
+            $result[] = $row;
+        }
+        return $result;
+    }
+
+}
