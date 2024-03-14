c55b00f3b11a330597533b27057d4fe7f5e593ae
LAS_Chanh <thanhnguyenhoang171official@gmail.com>
2024-03-14 16:26:52 +0700

    da xong phan dangnhap.php

--- diemthi/admin/dangnhap.php
+++ diemthi/admin/dangnhap.php
@@ -1,66 +1,81 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html
+	PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
+
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Đăng Nhập</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>Đăng Nhập</title>
 </head>
+
 <body>
-<div class="banner">
-<center><img src="../assets/img/Ban.gif"></center>
-<body bgcolor="#CAFFFF">
-<?php
-echo '<br/>';
-?>
-<?php
-?>
-<style type="text/css">
-#menu ul{
-	margin-left:145px;
-	text-align: center;
+	<div class="banner">
+		<center><img src="../assets/img/Ban.png" width="100%" height="250px"></center>
 
-}
-.menu{
+		<body bgcolor="#CAFFFF">
+			<?php
+			echo '<br/>';
+			?>
+			<?php
+			?>
+			<style type="text/css">
+				#menu ul {
+					margin-left: 14.9vw;
+					padding-bottom: 40vh;
+					text-align: center;
 
-	}
-#menu ul li{
-display:inline;
+				}
 
-}
-#menu ul a{
-text-decoration:none;
-	width: 490px;
-float:left;
-background:#336699;
-color:#FFFFFF;
-text-align:center;
-line-height:27px;
-font-weight:bold;
-border-left:1px solid #FFFFFF;
-}
+				.menu {}
 
-#menu ul a:hover{
-background:#000000;
-}
-</style>
-<link rel="stylesheet" type="text/css" href="style.css">
-<div id="menu" >
-		<p style="font-family:Tahoma;font-weight: bold;text-align: center;font-size: large">CHÀO MỪNG BẠN ĐẾN TRANG ĐĂNG NHẬP</p>
-    	<ul>
-        	<li><a href="login.php">Đăng Nhập Quản Trị Hệ Thống</a></li>
-			<li><a href="logingv.php">Đăng Nhập Giáo Viên</a></li>
-		</ul>
-</div>
-<table  border=0 cellpadding=5 cellspacing=0 align="center" width="983">
-	<TR>
-		<TD>	<tr>
-		<td  colspan="2" bgcolor="#336699" align="center" style="color:white" >
-		 Copyright &copy; 20016 Trường THPT Trần Khai Nguyên <br>		
-		</td>
-	</tr>
-</td>
-	</TR>
-</table>
+				#menu ul li {
+					display: inline;
+
+				}
+
+				#menu ul a {
+					text-decoration: none;
+					width: 490px;
+					float: left;
+					background: #336699;
+					color: #FFFFFF;
+					text-align: center;
+					line-height: 27px;
+					font-weight: bold;
+					border-left: 1px solid #FFFFFF;
+				}
+
+				#menu ul a:hover {
+					background: #000000;
+				}
+			</style>
+			<link rel="stylesheet" type="text/css" href="style.css">
+			<div id="menu">
+				<p style="font-family:Tahoma;font-weight: bold;text-align: center;font-size: large;color:#FF3300;">CHÀO
+					MỪNG BẠN ĐẾN
+					TRANG ĐĂNG NHẬP</p>
+				<p
+					style="font-family: Tahoma; font-style: italic; text-align: center; font-size: large; color: #F1F1F1">
+					Hãy chọn loại
+					tài khoản để đăng nhập</p>
+
+				<ul>
+					<li><a href="login.php">ADMIN</a></li>
+					<li><a href="logingv.php">Giảng Viên</a></li>
+				</ul>
+			</div>
+			<table border=0 cellpadding=5 cellspacing=0 align="center" width="983">
+				<TR>
+					<TD>
+				<tr>
+					<td colspan="2" bgcolor="#336699" align="center" style="color:white">
+						@ 2024 - Hoàng Thanh & Minh Khuê Open University @<br>
+					</td>
+				</tr>
+				</td>
+				</TR>
+			</table>
+		</body>
+
 </body>
 
-</body>
 </html>
